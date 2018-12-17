1b033957f6fdf98123613776a56fa3e9475c2694
wuyahuang <wuyahuang@gmail.com>
2018-12-17 20:11:47 +0800

    update README.md

--- testnet/deploy_system_contract.sh
+++ testnet/deploy_system_contract.sh
@@ -3,7 +3,7 @@ cleos set contract eosio.token ./ eosio.token.wasm eosio.token.abi -p eosio.toke
 cleos push action eosio.token create '[ "eosio", "10000000000.0000 MEETONE"]' -p eosio.token@active
 cleos push action eosio.token issue '[ "eosio", "10000000000.0000 MEETONE", "memo" ]' -p eosio@active
 cleos push action eosio.token transfer '[ "eosio", "eosio.faucet", "10000000.0000 MEETONE", "" ]' -p eosio@active
-cleos push action eosio.token transfer '[ "eosio", "m", "90000000.0000 MEETONE", "" ]' -p eosio@active
+cleos push action eosio.token transfer '[ "eosio", "m", "900000000.0000 MEETONE", "" ]' -p eosio@active
 cleos get currency balance eosio.token eosio
 cleos get currency balance eosio.token eosio.faucet
 
