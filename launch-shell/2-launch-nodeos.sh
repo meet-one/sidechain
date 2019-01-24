a7451b991857b1f14f163b0395f769c288892535
wuyahuang <wuyahuang@gmail.com>
2019-01-24 15:28:35 +0800

    remove max-implicit-request

--- launch-shell/2-launch-nodeos.sh
+++ launch-shell/2-launch-nodeos.sh
@@ -1,6 +1,7 @@
 # 2. ABP launch nodeos
 
 cd ~
+mkdir nodeos
 mkdir nodeos/config-dir
 mkdir nodeos/data-dir
 mkdir shell
@@ -9,4 +10,3 @@ wget https://raw.githubusercontent.com/meet-one/sidechain/master/mainnet/config.
 wget https://raw.githubusercontent.com/meet-one/sidechain/master/mainnet/genesis.json
 cd ~/shell
 nohup /usr/local/eosio/bin/nodeos --config-dir ~/nodeos/config-dir --data-dir ~/nodeos/data-dir --genesis-json ~/nodeos/config-dir/genesis.json  > log-nodeos.log 2>&1 &
-
