c75c5e255626fdce77cb39f1a83da134d9eb3c42
wuyahuang <wuyahuang@gmail.com>
2019-01-29 15:43:46 +0800

    update production launch shell

--- launch/2-launch-nodeos.sh
+++ launch/2-launch-nodeos.sh
@@ -7,7 +7,7 @@ mkdir nodeos/config-dir
 mkdir nodeos/data-dir
 mkdir shell
 cd nodeos/config-dir
-wget https://raw.githubusercontent.com/meet-one/sidechain/master/mainnet/config.ini
-wget https://raw.githubusercontent.com/meet-one/sidechain/master/mainnet/genesis.json
+wget https://raw.githubusercontent.com/meet-one/sidechain/master/launch/mainnet/config.ini
+wget https://raw.githubusercontent.com/meet-one/sidechain/master/launch/mainnet/genesis.json
 cd ~/shell
 nohup /usr/local/eosio/bin/nodeos --config-dir ~/nodeos/config-dir --data-dir ~/nodeos/data-dir --genesis-json ~/nodeos/config-dir/genesis.json  > log-nodeos.log 2>&1 &
