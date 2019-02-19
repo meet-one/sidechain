ee9617a20e8d2658605429473d305179c48202d0
wuyahuang <wuyahuang@gmail.com>
2019-02-19 10:39:08 +0800

    update genesis launch shell.

---
+++ genesis/2-launch-nodeos.sh
@@ -0,0 +1,13 @@
+# 2. Genesis BP launch nodeos
+
+cd ~
+rm -rf nodeos
+mkdir nodeos
+mkdir nodeos/config-dir
+mkdir nodeos/data-dir
+mkdir shell
+cd nodeos/config-dir
+wget https://raw.githubusercontent.com/meet-one/sidechain/master/genesis/config-dir/config.ini
+wget https://raw.githubusercontent.com/meet-one/sidechain/master/genesis/config-dir/genesis.json
+cd ~/shell
+nohup nodeos --config-dir ~/nodeos/config-dir --data-dir ~/nodeos/data-dir --genesis-json ~/nodeos/config-dir/genesis.json > log-nodeos.log 2>&1 &
