5a68b34e87c5284758b30f4a341f2a03e1ede1a0
UMU618 <umu618@hotmail.com>
2019-02-28 16:24:27 +0800

    + airgrab script

---
+++ snapshot@2019-02-19/1-airgrab-export-accounts.js
@@ -0,0 +1,96 @@
+#!/usr/bin/env node
+
+/**
+ * @author UMU618 <umu618@hotmail.com>
+ * @copyright MEET.ONE 2019
+ * @description Use npm-coding-style.
+ * yarn add eosjs
+ * yarn add moment
+ */
+
+'use strict'
+
+// also support https
+const URL = 'http://fullnode.meet.one'
+const CODE = 'airgrab.m'
+const SCOPE = CODE
+const TABLE = 'accounts'
+const LIMIT = 10000
+
+const EosApi = require('eosjs-api')
+const options = {
+  httpEndpoint: URL
+  , verbose: false
+  , logger: {
+    log: null //console.log
+    , error: console.error
+  }
+  , fetchConfiguration: {}
+}
+const eos = EosApi(options)
+
+const moment = require('moment')
+const outputPath = '1-airgrab-accounts@'
+  + moment().format('YYYY-MM-DD[T]HH-mm-ss.SSS[Z]ZZ') + '.txt'
+console.log('OutputPath: ' + outputPath)
+
+const fs = require('fs')
+const ws = fs.createWriteStream(outputPath, { encoding: 'utf8', autoClose: true })
+
+const set = new Set()
+
+let lastOne = 0
+let retry = 0
+
+function succeeded(res) {
+  retry = 0
+  if (res.rows) {
+    let buffer = ''
+    res.rows.forEach(e => {
+      if (set.has(e.owner)) {
+        throw new  Error('Duplicated: ' + e.owner)
+      }
+      if (e.id > lastOne) {
+        lastOne = e.id
+      }
+      set.add(e.owner)
+      buffer += e.owner + '\n'
+    })
+    ws.write(buffer)
+  }
+  if (res.more) {
+    ++lastOne
+    console.log('Next: ' + lastOne)
+
+    eos
+      .getTableRows(
+        {json: true, code: CODE, scope: SCOPE, table: TABLE, limit: LIMIT
+          , lower_bound: lastOne}
+      )
+      .then(succeeded, failed)
+  } else {
+    ws.end()
+    console.log('Done!')
+  }
+}
+
+function failed(err) {
+  if (retry++ < 3) {
+    console.log('Retry on ' + lastOne + ' for ' + retry + ' time(s)')
+    eos
+      .getTableRows(
+        {json: true, code: CODE, scope: SCOPE, table: TABLE, limit: LIMIT
+          , lower_bound: lastOne}
+      )
+      .then(succeeded, failed)
+  } else {
+    console.log('Retry failed on ' + lastOne)
+  }
+}
+
+eos
+  .getTableRows(
+    {json: true, code: CODE, scope: SCOPE, table: TABLE, limit: LIMIT
+      , lower_bound: lastOne}
+  )
+  .then(succeeded, failed)
---
+++ snapshot@2019-02-19/2-airgrab-create-transfer.js
@@ -0,0 +1,175 @@
+#!/usr/bin/env node
+
+/**
+ * @author UMU618 <umu618@hotmail.com>
+ * @copyright MEET.ONE 2019
+ * @description Use npm-coding-style.
+ * yarn add commander
+ * yarn add eosjs
+ * yarn add moment
+ */
+
+'use strict'
+
+const SEPARATOR = ','
+
+const DEFAULT_OUTPUT_FILE_NAME = '2-airgrab-transfer.sh'
+
+let inputPath = ''
+let mapFilePath = ''
+let balanceFilePath = ''
+let outputPath = ''
+let payer = ''
+
+const eosfmt = require('eosjs').modules.format
+
+// parse arguments
+{
+  const po = require('commander')
+  po
+    .version('0.1.0')
+    .arguments('<input>', 'File that contains accounts list.')
+    .arguments('<map-file>', 'File that contains accounts map.')
+    .arguments('<balance-file>', 'File that contains accounts balance.')
+    .action(function (input, mapFile, balancePath) {
+      inputPath = input
+      mapFilePath = mapFile
+      balanceFilePath = balancePath
+    })
+    .option('-p, --payer <TEXT>', 'Set payer')
+    .option('-o, --output <FILE>', 'Write to FILE, will be overwritten!')
+    .on('--help', function () {
+    })
+    .parse(process.argv)
+
+  if (!inputPath) {
+    po.outputHelp()
+    process.exit(-1)
+  }
+
+  console.log('Input file: ' + inputPath)
+  console.log('Map file: ' + mapFilePath)
+  console.log('Balance file: ' + balanceFilePath)
+
+  if (po.payer) {
+    payer = po.payer
+    // check payer name
+    if (!eosfmt.isName(payer)) {
+      console.log('Payer: ' + payer + ' is not an invalid account name.')
+      process.exit(-1)
+    }
+  } else {
+    payer = 'airgrab.m'
+  }
+  console.log('Payer: ' + payer)
+
+  if (po.output) {
+    outputPath = po.output
+  } else {
+    outputPath = DEFAULT_OUTPUT_FILE_NAME
+  }
+  console.log('Output file: ' + outputPath)
+}
+
+const fs = require('fs')
+const readline = require('readline')
+
+// key = sidechain account, value = mainnet account
+const map = new Map()
+const balanceMap = new Map()
+
+readMap()
+
+function readMap() {
+  const rs = fs.createReadStream(mapFilePath
+    , {encoding: 'utf8', autoClose: true}
+  )
+  const rl = readline.createInterface({input: rs, crlfDelay: Infinity})
+
+  rl.on('line', (line) => {
+    let e = line.split(SEPARATOR)
+    if (e.length == 2) {
+      // UMU：已经验证过了，注释掉是为了提高运行效率！
+      // if (!eosfmt.isName(e[0])) {
+      //   console.error('Invalid account name: ' + e[0])
+      // } else if (!eosfmt.isName(e[1])) {
+      //   console.error('Invalid account name: ' + e[1])
+      // } else {
+         map.set(e[1], e[0])
+      // }
+    }
+  })
+
+  rl.on('close', () => {
+    console.log('Map size: ' + map.size)
+    readBalance()
+  })
+}
+
+function readBalance() {
+  const rs = fs.createReadStream(balanceFilePath
+    , {encoding: 'utf8', autoClose: true}
+  )
+  const rl = readline.createInterface({input: rs, crlfDelay: Infinity})
+
+  rl.on('line', (line) => {
+    let e = line.split(SEPARATOR)
+    if (e.length == 2) {
+      // UMU：已经验证过了，注释掉是为了提高运行效率！
+      // if (eosfmt.isName(e[0])) {
+         balanceMap.set(e[0], parseFloat(e[1]))
+      // } else {
+      //   console.error('Invalid account name: ' + e[0])
+      // }
+    }
+  })
+
+  rl.on('close', () => {
+    console.log('Balance size: ' + balanceMap.size)
+    createShellScript()
+  })
+}
+
+function createShellScript() {
+  const rs = fs.createReadStream(inputPath
+    , { encoding: 'utf8', autoClose: true }
+  )
+  const rl = readline.createInterface({ input: rs, crlfDelay: Infinity })
+
+  const ws = fs.createWriteStream(outputPath
+    , { flags: 'w', encoding: 'utf8', autoClose: true }
+  )
+
+  ws.write('echo Run "cleos wallet unlock" first\n')
+  ws.write('payer="' + payer + '"\n')
+
+  rl.on('line', (line) => {
+    if (eosfmt.isName(line)) {
+      if (map.has(line)) {
+        let mainnetAccount = map.get(line)
+        if (balanceMap.has(mainnetAccount)) {
+          let mainnetBalance = balanceMap.get(mainnetAccount)
+          let balance = (mainnetBalance / 2).toFixed(4)
+          if (balance == 0.0) {
+            console.log('Account has no balance: ' + line + ', '
+              + mainnetAccount)
+          } else {
+            ws.write('cleos transfer $payer ' + line + ' "' + balance
+              + ' MEETONE" "airgrab: ' + mainnetAccount + ','
+              + mainnetBalance.toFixed(4) + ' EOS"\n')
+          }
+        } else {
+          console.error('Account name not in balance-file: ' + line)
+        }
+      } else {
+        console.error('Account name not in map-file: ' + line)
+      }
+    } else {
+      console.error('Invalid account name: ' + line)
+    }
+  })
+
+  rl.on('close', () => {
+    ws.close()
+  })
+}
